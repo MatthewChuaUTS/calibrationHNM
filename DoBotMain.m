553fcd66ddf5aa7e3c099160a0f4574967d9e1cd
AceOf5pade5 <creativenerf@gmail.com>
2024-10-24 00:26:05 +1100

    Added a checklist and other comments

--- DoBotMain.m
+++ DoBotMain.m
@@ -1,3 +1,26 @@
+%% PROJECT CHECKLIST
+
+% - [ ] test initialising ROS
+% - [ ] test GetBasePos
+% - [ ] add calibration code
+% - [ ] manually record calibration endeffector positions
+% - [ ] populate moveArray with those positions
+% - [ ] test the calibration
+% - [ ] test sendTargetEndEffectorPose() with a valid position
+% - [ ] check what happens if you try to send an invalid position (plz don't break)
+% - [ ] test the VisualServoing() function.
+
+%% POSSIBLE THINGS THAT MAY GO WRONG
+
+% >>> nothing (PLEASE I NEED THIS)
+% >>> the movement functions can't access something that I should have made global
+% >>> the calibration doesn't work
+% >>> we don't actually end up using calibration like the tutors want
+% >>> the visual servoing just doesn't work (very not good, worst case scenario)
+
+
+%% ACTUAL CODE
+
 InitialiseRos();
 basePosImg = GetBasePos();
 cam = webcam;  % Connect to the default webcam
